9d5c8a529cf105f681091d93d8506143742f97d6
Herman Chien <Herman@fet.vue>
2022-04-07 10:49:23 +0800

    way1 to control class

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+	data() { return{
+			div1Selected: false,
+			div2Selected: false,
+			div3Selected: false
+	} },
+    methods: {
+		divSelected(id) {
+			if (id === 1) {
+				this.div1Selected = !this.div1Selected;
+			} else if (id === 2) {
+				this.div2Selected = !this.div2Selected;
+			} else if (id === 3) {
+				this.div3Selected = !this.div3Selected;
+			}
+		}
+	}
+})
+app.mount("#app")
